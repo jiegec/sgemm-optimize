e92a8e2ca446502f568e41c97dd57358cee40049
Jiajie Chen <c@jia.je>
2021-10-08 12:50:54 +0800

    Add PMU metrics

--- sgemm/run.sh
+++ sgemm/run.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "Running $1"
-srun -n1 --exclusive ./$1 | tee $1.log
+#srun -n1 --exclusive ./$1 | tee $1.log
+srun -n1 --exclusive perf stat -e L1-dcache-loads,L1-dcache-load-misses,cycles,instructions ./$1 2>&1 | tee $1.log
