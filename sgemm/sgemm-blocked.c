23c5e426aac9473fcf367a8d8d3043dbf5630458
Jiajie Chen <c@jia.je>
2021-10-09 10:42:34 +0800

    Helper for constant sized matrix

--- sgemm/sgemm-blocked.c
+++ sgemm/sgemm-blocked.c
@@ -39,6 +39,38 @@ static void do_block(int lda, int M, int N, int K, float *restrict A, float *res
   }
 }
 
+// let compiler optimize for M = N = BLOCK_SIZE
+static void do_block_constant_size(int lda, int M, int N, int K, float *restrict A, float *restrict B, float *restrict C)
+{
+  M = N = BLOCK_SIZE;
+  // pack B to continuous memory in cache
+  float BB[BLOCK_SIZE * BLOCK_SIZE];
+  for (int j = 0; j < N; ++j)
+  {
+    for (int k = 0; k < K; ++k)
+    {
+      BB[k + j * BLOCK_SIZE] = B[k + j * lda];
+    }
+  }
+
+  for (int k = 0; k < K; ++k)
+  {
+    /* For each column j of B */
+    for (int j = 0; j < N; ++j)
+    {
+      /* For each row i of A */
+      for (int i = 0; i < M; ++i)
+      {
+        /* Compute C(i,j) */
+        float cij = C[i + j * BLOCK_SIZE];
+        //cij += A[i + k * lda] * B[k + j * lda];
+        cij += A[i + k * lda] * BB[k + j * BLOCK_SIZE];
+        C[i + j * BLOCK_SIZE] = cij;
+      }
+    }
+  }
+}
+
 /* This routine performs a sgemm operation
  *  C := C + A * B
  * where A, B, and C are lda-by-lda matrices stored in column-major format. 
@@ -65,14 +97,27 @@ void square_sgemm(int lda, float *restrict A, float *restrict B, float *restrict
       }
 
       /* Accumulate block sgemms into block of C */
-      for (int k = 0; k < lda; k += BLOCK_SIZE)
+      if (M == BLOCK_SIZE && N == BLOCK_SIZE)
       {
-        /* Correct block dimensions if block "goes off edge of" the matrix */
-        int K = min(BLOCK_SIZE, lda - k);
+        for (int k = 0; k < lda; k += BLOCK_SIZE)
+        {
+          /* Correct block dimensions if block "goes off edge of" the matrix */
+          int K = min(BLOCK_SIZE, lda - k);
 
-        /* Perform individual block sgemm */
-        //do_block(lda, M, N, K, A + i + k * lda, B + k + j * lda, C + i + j * lda);
-        do_block(lda, M, N, K, A + i + k * lda, B + k + j * lda, CC);
+          /* Perform individual block sgemm */
+          do_block_constant_size(lda, M, N, K, A + i + k * lda, B + k + j * lda, CC);
+        }
+      }
+      else
+      {
+        for (int k = 0; k < lda; k += BLOCK_SIZE)
+        {
+          /* Correct block dimensions if block "goes off edge of" the matrix */
+          int K = min(BLOCK_SIZE, lda - k);
+
+          /* Perform individual block sgemm */
+          do_block(lda, M, N, K, A + i + k * lda, B + k + j * lda, CC);
+        }
       }
 
       // unpack
