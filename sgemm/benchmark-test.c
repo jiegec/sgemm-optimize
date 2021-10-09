f56890c2d3ab16408d6e44c53fd1d1e2411fc26e
Jiajie Chen <c@jia.je>
2021-10-09 15:24:09 +0800

    Revert unwanted change

--- sgemm/benchmark-test.c
+++ sgemm/benchmark-test.c
@@ -163,11 +163,8 @@ int main (int argc, char **argv)
 
     /* If any element in C is positive, then something went wrong in square_sgemm */
     for (int i = 0; i < n * n; ++i)
-      if (C[i] > initial) {
-
-printf("%d %d\n", i % n,i / n);
+      if (C[i] > initial)
 	die("*** FAILURE *** Error in matrix multiply exceeds componentwise error bounds.\n" );
-      }
   }
 
   free (buf);
